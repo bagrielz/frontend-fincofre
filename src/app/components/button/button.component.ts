546099036482512dcb4060b080cc439d40a62ec4
Gabriel Stênio <gabri.stnio50@hotmail.com>
2025-05-14 11:13:23 -0300

    feat: add the header component, logo and log-out button

---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,14 @@
+import { Component, input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+type ButtonType = 'primary' | 'secondary';
+
+@Component({
+  selector: 'app-button',
+  imports: [CommonModule],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.css',
+})
+export class ButtonComponent {
+  type = input.required<ButtonType>();
+}
