c5d246a3754c648a468773c9ffcc4351a71ba828
Gabriel Stênio <gabri.stnio50@hotmail.com>
2025-05-15 09:06:57 -0300

    feat: adds the title component in dashboard component

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { TitleComponent } from './title/title.component';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [TitleComponent],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrl: './dashboard.component.css',
 })
-export class DashboardComponent {
-
-}
+export class DashboardComponent {}
