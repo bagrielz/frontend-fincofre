60220df228ca00af526eb62c7743b0d130395e92
Gabriel Stênio <gabri.stnio50@hotmail.com>
2025-05-16 10:51:38 -0300

    style: adds the style to the table container

--- src/app/components/dashboard/table/table.component.ts
+++ src/app/components/dashboard/table/table.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { TitleComponent } from '../title/title.component';
 import { PrimaryButtonComponent } from '../../shared/primary-button/primary-button.component';
+import { SecondaryButtonComponent } from '../../shared/secondary-button/secondary-button.component';
+import { TabsComponent } from '../tabs/tabs.component';
 
 @Component({
   selector: 'app-table',
-  imports: [TitleComponent, PrimaryButtonComponent],
+  imports: [
+    TabsComponent,
+    TitleComponent,
+    PrimaryButtonComponent,
+    SecondaryButtonComponent,
+  ],
   templateUrl: './table.component.html',
   styleUrl: './table.component.css',
 })
