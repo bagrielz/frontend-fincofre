c91056414da655763f9dcd53269737edeea3316f
Gabriel Stênio <gabri.stnio50@hotmail.com>
2025-05-16 14:51:00 -0300

    feat: adds the tabs component and single tab component

--- src/app/components/dashboard/tabs/tabs.component.ts
+++ src/app/components/dashboard/tabs/tabs.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { SingleTabComponent } from './single-tab/single-tab.component';
 
 @Component({
   selector: 'app-tabs',
-  imports: [],
+  imports: [SingleTabComponent],
   templateUrl: './tabs.component.html',
-  styleUrl: './tabs.component.css'
+  styleUrl: './tabs.component.css',
 })
-export class TabsComponent {
-
-}
+export class TabsComponent {}
