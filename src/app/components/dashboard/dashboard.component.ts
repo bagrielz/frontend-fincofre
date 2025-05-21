15634184ea0b2a3aed826c041b4968cd9554762f
Gabriel Stênio <gabri.stnio50@hotmail.com>
2025-05-21 14:15:24 -0300

    style: adds the viewport height

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -2,10 +2,16 @@ import { Component } from '@angular/core';
 import { TableComponent } from './table/table.component';
 import { TitleComponent } from './title/title.component';
 import { PrimaryButtonComponent } from '../shared/button/primary-button/primary-button.component';
+import { ToastComponent } from '../shared/toast/toast.component';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [TableComponent, TitleComponent, PrimaryButtonComponent],
+  imports: [
+    TableComponent,
+    TitleComponent,
+    PrimaryButtonComponent,
+    ToastComponent,
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
