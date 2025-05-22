e192c9dbee28a598b951c88428dcf9dc4df2b0a4
Gabriel Stênio <gabri.stnio50@hotmail.com>
2025-05-22 13:43:40 -0300

    style: updates the style to fix the toast component to the page footer

--- src/app/components/shared/toast/toast.component.ts
+++ src/app/components/shared/toast/toast.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { PrimaryButtonComponent } from '../button/primary-button/primary-button.component';
 import { ButtonComponent } from '../button/button.component';
 
 @Component({
   selector: 'app-toast',
-  imports: [PrimaryButtonComponent, ButtonComponent],
+  imports: [ButtonComponent],
   templateUrl: './toast.component.html',
   styleUrl: './toast.component.css',
 })
