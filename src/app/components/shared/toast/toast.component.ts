4643bc582a9ca6c83141d0c7bd2e1d28f22aab96
Gabriel Stênio <gabri.stnio50@hotmail.com>
2025-05-22 08:55:39 -0300

    feat: adds the toast component

--- src/app/components/shared/toast/toast.component.ts
+++ src/app/components/shared/toast/toast.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { SecondaryButtonComponent } from '../button/secondary-button/secondary-button.component';
+import { PrimaryButtonComponent } from '../button/primary-button/primary-button.component';
 
 @Component({
   selector: 'app-toast',
-  imports: [],
+  imports: [SecondaryButtonComponent, PrimaryButtonComponent],
   templateUrl: './toast.component.html',
-  styleUrl: './toast.component.css'
+  styleUrl: './toast.component.css',
 })
-export class ToastComponent {
-
-}
+export class ToastComponent {}
