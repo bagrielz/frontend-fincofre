ca4fd28903d09f14eb15b25689c22ceee4019694
Gabriel Stênio <gabri.stnio50@hotmail.com>
2025-05-22 10:25:36 -0300

    ref: updates the logic to use the buttons type

--- src/app/components/shared/toast/toast.component.ts
+++ src/app/components/shared/toast/toast.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { SecondaryButtonComponent } from '../button/secondary-button/secondary-button.component';
 import { PrimaryButtonComponent } from '../button/primary-button/primary-button.component';
+import { ButtonComponent } from '../button/button.component';
 
 @Component({
   selector: 'app-toast',
-  imports: [SecondaryButtonComponent, PrimaryButtonComponent],
+  imports: [PrimaryButtonComponent, ButtonComponent],
   templateUrl: './toast.component.html',
   styleUrl: './toast.component.css',
 })
