453eedf6cd0bf22b6c735262597f8e110b2da849
Gabriel Stênio <gabri.stnio50@hotmail.com>
2025-06-03 15:21:02 -0300

    style: updates the title in add spent form component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,7 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './features/header/pages/header.component';
-import { DashboardComponent } from './features/dashboard/pages/dashboard.component';
 import { RouterOutlet } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
-import { AddSpentFormComponent } from './features/add-spent-form/pages/add-spent-form.component';
 
 const MODULES = [RouterOutlet, ReactiveFormsModule];
 
