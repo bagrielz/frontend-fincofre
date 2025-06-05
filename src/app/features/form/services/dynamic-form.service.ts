69f50dbf2d0a3497e79cc4501e10718afd3118a5
Gabriel Stênio <gabri.stnio50@hotmail.com>
2025-06-05 09:25:25 -0300

    feat: adds the functions to the dynamic form service

--- src/app/features/form/services/dynamic-form.service.ts
+++ src/app/features/form/services/dynamic-form.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { AbstractControlOptions, FormBuilder, FormGroup } from '@angular/forms';
+import { FormConfig } from '../../../shared/models/form-config.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DynamicFormService {
+  private formConfigs: { [key: string]: Function } = {};
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
+
+  registerFormConfig(formName: string, config: Function) {
+    this.formConfigs[formName] = config;
+  }
+
+  getFormConfig(formKey: string, ...args: any[]): FormConfig {
+    if (!this.formConfigs[formKey]) {
+      throw new Error(`Configuração de formulário não encontrada: ${formKey}`);
+    }
+
+    return this.formConfigs[formKey](...args);
+  }
+
+  createFormGroup(
+    config: FormConfig,
+    formOptions?: AbstractControlOptions
+  ): FormGroup {
+    const formControls: { [key: string]: any } = {};
+
+    config.fields.forEach((field) => {
+      formControls[field.formControlName] = ['', field.validators];
+    });
+
+    return this.fb.group(formControls, formOptions);
+  }
 }
