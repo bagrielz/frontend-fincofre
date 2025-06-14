31683b63465164dcbf23344054bd548492e53dfb
Gabriel Stênio <gabri.stnio50@hotmail.com>
2025-06-14 14:59:25 -0300

    feat: adds the pill interface

---
+++ src/app/components/pill/pill.component.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pill',
+  imports: [CommonModule],
+  templateUrl: './pill.component.html',
+  styleUrl: './pill.component.css',
+})
+export class PillComponent {}
---
+++ src/app/shared/models/pill.interface.ts
@@ -0,0 +1,4 @@
+export interface Pill {
+  text: string;
+  color: string;
+}
