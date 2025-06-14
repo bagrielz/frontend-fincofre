4ece228341efff26e033274d890eb8458c8527da
Gabriel Stênio <gabri.stnio50@hotmail.com>
2025-06-14 15:52:05 -0300

    style: remove commented lines

--- src/app/features/form/components/select/select.component.ts
+++ src/app/features/form/components/select/select.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { PillComponent } from '../../../../components/pill/pill.component';
 
 @Component({
   selector: 'app-select',
   imports: [],
   templateUrl: './select.component.html',
-  styleUrl: './select.component.css'
+  styleUrl: './select.component.css',
 })
-export class SelectComponent {
-
-}
+export class SelectComponent {}
