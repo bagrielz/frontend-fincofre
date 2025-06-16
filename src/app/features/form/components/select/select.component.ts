f6bbd201eb265dc1f65d601c968d25f44b91b9f0
Gabriel Stênio <gabri.stnio50@hotmail.com>
2025-06-16 14:41:18 -0300

    feat: adds the logic to the show the list options

--- src/app/features/form/components/select/select.component.ts
+++ src/app/features/form/components/select/select.component.ts
@@ -1,11 +1,12 @@
-import { Component, signal } from '@angular/core';
+import { Component, ElementRef, signal, ViewChild } from '@angular/core';
 import { PillComponent } from '../../../../components/pill/pill.component';
 import { PillConfig } from '../../../../shared/models/pill-config.interface';
 import { PillStatus } from '../../../../core/pill-status.config';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-select',
-  imports: [PillComponent],
+  imports: [PillComponent, CommonModule],
   templateUrl: './select.component.html',
   styleUrl: './select.component.css',
 })
@@ -14,7 +15,14 @@ export class SelectComponent {
 
   selectedPill = signal<PillConfig | null>(null);
 
+  @ViewChild('optionsViewButton', { static: false })
+  optionsViewButtonRef!: ElementRef;
+
   selectPill(pill: PillConfig) {
     this.selectedPill.set(pill);
+
+    if (this.optionsViewButtonRef) {
+      this.optionsViewButtonRef.nativeElement.checked = false;
+    }
   }
 }
