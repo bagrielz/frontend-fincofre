e0581022df559c4d069cd9cf81fc2d45932ce2dd
Gabriel Stênio <gabri.stnio50@hotmail.com>
2025-06-20 17:15:49 -0300

    feat: adds custom-list component

---
+++ src/app/features/form/components/custom-list/custom-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-list',
+  imports: [],
+  templateUrl: './custom-list.component.html',
+  styleUrl: './custom-list.component.css'
+})
+export class CustomListComponent {
+
+}
