89b0081bfb80aee03a530067917ea71681fe9253
Gabriel Stênio <gabri.stnio50@hotmail.com>
2025-06-24 14:28:08 -0300

    feat: adds the login component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './features/dashboard/pages/dashboard.component';
 import { AddSpentFormComponent } from './features/form/pages/add-spent-form/add-spent-form.component';
+import { LoginComponent } from './features/login/pages/login.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/inicio', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
   { path: 'inicio', component: DashboardComponent },
   { path: 'adicionar-gasto', component: AddSpentFormComponent },
 ];
