3be119262725f49a4ee23fecf094813604f606ed
Gabriel Stênio <gabri.stnio50@hotmail.com>
2025-06-25 08:53:53 -0300

    feat: adds the initial structure of the login page

--- src/app/features/login/pages/login.component.ts
+++ src/app/features/login/pages/login.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../../../shared/components/button/button.component';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
-export class LoginComponent {
-
-}
+export class LoginComponent {}
