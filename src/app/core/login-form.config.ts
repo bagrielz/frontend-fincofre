b375e3491594bebe3e4a193b312c3062ddb29c78
Gabriel Stênio <gabri.stnio50@hotmail.com>
2025-06-27 09:22:58 -0300

    feat: adds the values to the login form config

--- src/app/core/login-form.config.ts
+++ src/app/core/login-form.config.ts
@@ -1,3 +1,4 @@
+import { Validators } from '@angular/forms';
 import { FormConfig } from '../shared/models/form-config.interface';
 
 export function getLoginForm(): FormConfig {
@@ -8,6 +9,34 @@ export function getLoginForm(): FormConfig {
       {
         type: 'primary',
         text: 'Entrar',
+      },
+      {
+        type: 'secondary',
+        text: 'Criar uma conta gratuita',
+      },
+    ],
+    fields: [
+      {
+        label: 'E-mail',
+        formControlName: 'email',
+        type: 'email',
+        required: true,
+        errorMessages: {
+          required: 'E-mail é um campo obrigatório',
+        },
+        placeholder: 'Digite o seu e-mail',
+        validators: [Validators.required],
+      },
+      {
+        label: 'Senha',
+        formControlName: 'password',
+        type: 'password',
+        required: true,
+        errorMessages: {
+          required: 'Senha é um campo obrigatório',
+        },
+        placeholder: 'Digite a sua senha',
+        validators: [Validators.required],
       },
     ],
   };
