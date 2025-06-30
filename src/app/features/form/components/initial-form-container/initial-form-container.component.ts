677d55a1a355eaeb7bbf8af60e301feca2c5fe9a
Gabriel Stênio <gabri.stnio50@hotmail.com>
2025-06-30 08:41:32 -0300

    feat: adds the logic to the mount login form and updates the form config

--- src/app/features/form/components/initial-form-container/initial-form-container.component.ts
+++ src/app/features/form/components/initial-form-container/initial-form-container.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { ButtonComponent } from '../../../../shared/components/button/button.component';
 import { FormConfig } from '../../../../shared/models/form-config.interface';
 import { FormGroup } from '@angular/forms';
+import { DynamicFormService } from '../../services/dynamic-form.service';
+import { getLoginForm } from '../../../../core/login-form.config';
 
 @Component({
   selector: 'app-initial-form-container',
@@ -12,4 +14,13 @@ import { FormGroup } from '@angular/forms';
 export class InitialFormContainerComponent {
   loginForm!: FormGroup;
   formConfig!: FormConfig;
+
+  constructor(private dynamicFormService: DynamicFormService) {
+    this.dynamicFormService.registerFormConfig('loginForm', getLoginForm);
+  }
+
+  ngOnInit(): void {
+    this.formConfig = this.dynamicFormService.getFormConfig('loginForm');
+    this.loginForm = this.dynamicFormService.createFormGroup(this.formConfig);
+  }
 }
