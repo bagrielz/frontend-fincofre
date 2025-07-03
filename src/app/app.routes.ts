9d3d34988ba55d9b1260a77b997abbdbca57e205
Gabriel Stênio <gabri.stnio50@hotmail.com>
2025-07-03 08:45:24 -0300

    feat: adds the register route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from './features/dashboard/pages/dashboard.component';
 import { AddSpentFormComponent } from './features/form/pages/add-spent-form/add-spent-form.component';
 import { LoginFormComponent } from './features/form/pages/login-form/login-form.component';
+import { RegisterFormComponent } from './features/form/pages/register-form/register-form.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/inicio', pathMatch: 'full' },
   { path: 'login', component: LoginFormComponent },
+  { path: 'cadastrar', component: RegisterFormComponent },
   { path: 'inicio', component: DashboardComponent },
   { path: 'adicionar-gasto', component: AddSpentFormComponent },
 ];
