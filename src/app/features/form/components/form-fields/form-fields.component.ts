f1c2d80eae6aa11a0104413d4284043a465643d2
Gabriel Stênio <gabri.stnio50@hotmail.com>
2025-08-16 19:13:23 -0300

    ref: updates the logic for filling in profile data

--- src/app/features/form/components/form-fields/form-fields.component.ts
+++ src/app/features/form/components/form-fields/form-fields.component.ts
@@ -2,6 +2,11 @@ import { Component, input } from '@angular/core';
 import { FormFieldBase } from '../../../../shared/models/form-field-base.interface';
 import { CommonModule } from '@angular/common';
 import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormConfig } from '../../../../shared/models/form-config.interface';
+import { getUpdateSpentForm } from '../../../../core/update-spent-form.config';
+import { getProfileFormConfig } from '../../../../core/profile-form.config';
+
+let forms: Array<FormConfig> = [getUpdateSpentForm(), getProfileFormConfig()];
 
 @Component({
   selector: 'app-form-fields',
@@ -13,7 +18,9 @@ export class FormFieldsComponent {
   form = input.required<FormGroup>();
   fields = input.required<FormFieldBase[]>();
 
-  hasError(controlName: string): boolean {
+  hasError(controlName: string, field: FormFieldBase): boolean {
+    if (!field.validators) return false;
+
     const control = this.form().get(controlName);
     return !!(control && control.invalid && (control.dirty || control.touched));
   }
