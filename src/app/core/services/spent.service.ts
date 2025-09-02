ec8e1bc174ec101cb9194a1435d0214db3753f15
Gabriel Stênio <gabri.stnio50@hotmail.com>
2025-09-02 11:31:14 -0300

    feat: add getAllSpents in spent service

--- src/app/core/services/spent.service.ts
+++ src/app/core/services/spent.service.ts
@@ -2,6 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment.development';
+import { Spent } from '../../shared/models/spent.interface';
+
+function getHeaders(token: string | null): HttpHeaders {
+  return new HttpHeaders({
+    Authorization: `Bearer ${token}`,
+  });
+}
 
 @Injectable({
   providedIn: 'root',
@@ -10,4 +17,10 @@ export class SpentService {
   private apiUrl: string = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
+
+  getAllSpents(token: string | null): Observable<Spent> {
+    const headers = getHeaders(token);
+
+    return this.http.get<Spent>(`${this.apiUrl}/gastos/listar`, { headers });
+  }
 }
