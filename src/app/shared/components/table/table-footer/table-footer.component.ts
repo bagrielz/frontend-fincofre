1ec3f15ee1ba9bbf65390b8cb8b8ae2415b6850a
Gabriel Stênio <gabri.stnio50@hotmail.com>
2025-09-04 09:30:54 -0300

    feat: includes total spents in the table

--- src/app/shared/components/table/table-footer/table-footer.component.ts
+++ src/app/shared/components/table/table-footer/table-footer.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TokenService } from '../../../../core/services/token.service';
+import { SpentService } from '../../../../core/services/spent.service';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-table-footer',
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './table-footer.component.html',
-  styleUrl: './table-footer.component.css'
+  styleUrl: './table-footer.component.css',
 })
-export class TableFooterComponent {
+export class TableFooterComponent implements OnInit {
+  token!: string | null;
+  total!: number;
 
+  constructor(
+    private tokenService: TokenService,
+    private spentService: SpentService
+  ) {}
+
+  ngOnInit(): void {
+    this.token = this.tokenService.returnToken();
+    this.spentService.getAllSpents(this.token);
+
+    this.spentService.spentsResponse$.subscribe((res) => {
+      this.total = res.total;
+    });
+  }
 }
