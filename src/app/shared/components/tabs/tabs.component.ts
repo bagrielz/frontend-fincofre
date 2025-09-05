79a4695ebd0088f8b22ad6e939864eeec006e1b0
Gabriel Stênio <gabri.stnio50@hotmail.com>
2025-09-05 14:16:06 -0300

    ref: updates the data for the tabs and single-tab components

--- src/app/shared/components/tabs/tabs.component.ts
+++ src/app/shared/components/tabs/tabs.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, output } from '@angular/core';
 import { SingleTabComponent } from './single-tab/single-tab.component';
 
 @Component({
@@ -7,4 +7,14 @@ import { SingleTabComponent } from './single-tab/single-tab.component';
   templateUrl: './tabs.component.html',
   styleUrl: './tabs.component.css',
 })
-export class TabsComponent {}
+export class TabsComponent {
+  tabChange = output<string>();
+
+  tabs = ['Todos', 'Gastos fixos', 'Gastos variáveis'];
+  selectedTab = 0;
+
+  onTabSelected(type: string, index: number) {
+    this.selectedTab = index;
+    this.tabChange.emit(type);
+  }
+}
