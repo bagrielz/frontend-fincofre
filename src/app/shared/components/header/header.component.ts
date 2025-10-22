6e3d3fe3179f00d037a9e78d0a6b38834b3b684d
Gabriel Stênio <gabri.stnio50@hotmail.com>
2025-10-22 17:22:40 -0300

    feat: add the userLogout function in header component

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../button/button.component';
 import { Router } from '@angular/router';
+import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -9,9 +10,14 @@ import { Router } from '@angular/router';
   styleUrl: './header.component.css',
 })
 export class HeaderComponent {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private authService: AuthService) {}
 
   backToHome() {
     this.router.navigateByUrl('/inicio');
+  }
+
+  userLogout() {
+    this.authService.logout();
+    this.router.navigateByUrl('/login');
   }
 }
